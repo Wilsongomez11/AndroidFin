f415d0834c937068407f42855554a0d99d284b4b
Wilson Gomez <willynton11@gmail.com>
2025-05-20 11:20:46 -0500

    proyectoFinal

--- app/src/main/java/com/example/proyectofinal/Api/ApiClient.kt
+++ app/src/main/java/com/example/proyectofinal/Api/ApiClient.kt
@@ -1,26 +1,4 @@
-package com.example.proyectofinal.Apia
-
-import com.example.proyectofinal.ApiService
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+package com.example.proyectofinal.Api
 
 object ApiClient {
-
-    private const val BASE_URL = "http://10.0.2.2:8081"
-
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }).build())
-            .build()
-    }
-
-    val apiService: ApiService by lazy {
-        retrofit.create(ApiService::class.java)
-    }
 }
